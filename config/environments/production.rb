d5144167984863daf5d9b9ea359f777d289ccabc
Vlastik Walker <vlastikczech1@gmail.com>
2018-09-07 16:24:56 -0700

    Update production.rb (#20)
    should be
    ```  config.action_mailer.default_url_options = { host: 'firehose-fire.herokuapp.com' }
    ```

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.defaultl_url_options = { host: 'firehose-fire.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'firehose-fire.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
