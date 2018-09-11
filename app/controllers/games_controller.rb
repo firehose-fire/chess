9bd7fe863b7ae11ff702a6f9f8068c46500bd404
JESSE DESANTO <JESSESUS13@GMAIL.COM>
2018-09-11 16:20:11 -0700

    Add link to update available game with current user to user_white_id

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -11,5 +11,13 @@ class GamesController < ApplicationController
 
   def show
   end
+
+  def update
+    @game = Games.find(params[:id])
+
+    if @game.update(:user_white_id => current_user)
+      redirect_to @game
+    end
+  end
   
 end
