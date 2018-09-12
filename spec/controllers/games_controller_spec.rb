b45b495d13c45ad485a9ee3862602bcb60fd92cd
JESSE DESANTO <JESSESUS13@GMAIL.COM>
2018-09-12 13:06:25 -0700

    Still trying to fix test. Console shows code functioning as intended

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -7,14 +7,14 @@ RSpec.describe GamesController, type: :controller do
     it "should update available game user white id with current user id" do
       
       user = FactoryBot.create(:user)
-      
+      current_user = FactoryBot.create(:user)
       # sign_in user
 
-      game = FactoryBot.create(:game, user_black_id: '3')
+      game = FactoryBot.create(:game, user_black_id: user.id)
 
-      patch :update, params: { user_white_id: user.id}
+      patch :update, params: { user_white_id: current_user.id}
 
-      expect(user_white_id).to eq user.id
+      expect(user_white_id).to eq current_user.id
     end
 
   end
