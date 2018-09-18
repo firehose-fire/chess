b150a94d8edcd862f6801e5c4ea2154db754a23a
vlastik <vlastikczech1@gmail.com>
2018-09-18 09:22:52 -0700

    added the correct logic for piece.rb

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -7,17 +7,19 @@ class Piece < ApplicationRecord
   def captured?(new_x, new_y)
     target_move = Piece.where(coordinate_x: new_x, coordinate_y: new_y).first
 
-    if(target_move.user_id == self.user_id)
-      raise RuntimeError
-    else
-      target_move.update_attributes(coordinate_x: nil, coordinate_y: nil, captured: true)
+    if defined?(target_move.user_id)
+      if(target_move.user_id == self.user_id)
+        raise RuntimeError
+      else
+        target_move.update_attributes(coordinate_x: nil, coordinate_y: nil, captured: true)
+      end
     end
+
   end
 
 
   def move_to!(new_x, new_y)
-
-    captured?(new_x, new_y) ? update_attributes(coordinate_x: new_x, coordinate_y: new_y) : nil
+    captured?(new_x, new_y) ? nil : update_attributes(coordinate_x: new_x, coordinate_y: new_y)
   end
 
 
