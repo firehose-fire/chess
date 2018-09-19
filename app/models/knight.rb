75600a73b0ad296742d98a31aace277892bd8a17
JESSE DESANTO <JESSESUS13@GMAIL.COM>
2018-09-19 14:01:58 -0700

    Define valid_move? for all pieces

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,2 +1,7 @@
 class Knight < Piece
+
+  def valid_move?(to_coordinate_x, to_coordinate_y)
+
+  end
+
 end
--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,2 +1,7 @@
 class Rook < Piece
+
+  def valid_move?(to_coordinate_x, to_coordinate_y)
+    
+  end
+  
 end
