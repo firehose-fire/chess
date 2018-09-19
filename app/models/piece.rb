9fe1cd0ea164a766a101d9a07601a49cb8525852
Cory Hack <cory.hack@gmail.com>
2018-09-19 01:32:00 +0000

    Add is_obstructed method for pieces

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -2,4 +2,58 @@ class Piece < ApplicationRecord
   belongs_to :game
   belongs_to :user
 
+  def is_occupied?(x,y)
+    # search the pieces database and see if x, y are occupied 
+    game.pieces.where(coordinate_x: x, coordinate_y: y).present?
+  end
+
+# Define method using x and y target coordinates to see if move is horizontal
+  def is_path_horizontal?(x_position_change, y_target)
+      (1..x_position_change.abs).each do |x|
+        return true if is_occupied?(x, y_target)
+      end
+  end
+
+  # Define method using x and y target coordinates to see if move is vertical
+  def is_path_vertical?(x_target, y_position_change)
+      (1..y_position_change.abs).each do |y|
+        return true if is_occupied?(x_target, y)
+      end
+  end
+
+  # Define method using x and y target coordinates to see if move is diaganol
+  def is_path_diaganol?(x_position_change, y_position_change)
+      ((1..x_position_change)).each do |x|
+        ((1..y_position_change)).each do |y|
+          return true if is_occupied?(x, y)
+        end
+      end
+  end
+
+# Define method using x and y target coordinates to see if move is outside of the defined game board
+  def is_path_valid?(x_target, y_target)
+    if x_target > 7 || x_target < 0 || y_target > 7 || y_target < 0
+      return "invalid path"
+    end
+  end
+
+  def is_obstructed?(x_origin, y_origin, x_target, y_target)
+    x_position_change = (x_target - x_origin).abs
+    y_position_change = (y_target - y_origin).abs
+
+    # is the path valid
+    if is_path_valid?(x_target, y_target)
+      return "Out of bounds of box"
+    # is the path horizontal
+    elsif x_position_change > 0 && y_position_change == 0
+      is_path_horizontal?(x_position_change, y_target)
+    # is the path vertical
+    elsif x_position_change == 0 && y_position_change > 0
+      is_path_vertical?(x_target, y_position_change)
+    # is the path diaganol
+    elsif x_position_change == (y_position_change)
+      is_path_diaganol?(x_position_change, y_position_change) 
+    end
+  end
+
 end
--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Piece, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "is_obstructed?" do
+    it "should return true if there is a piece obstructing the vertical path"
+    user = FactoryBot.create(:user)
+    game = FactoryBot.create(:game)
+    piece1 = game.pieces.find_by(coordinate_x: 0 , coordinate_y: 0)
+    piece2 = game.pieces.find_by(coordinate_x: 0 , coordinate_y: 3)
+    
+    expected(result).to eq (true)
+  end
 end
