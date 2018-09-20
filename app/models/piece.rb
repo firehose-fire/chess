762b31c2e92049ae356b23693c16b58316ef7ef2
Cory Hack <cory.hack@gmail.com>
2018-09-20 05:13:12 +0000

    Add is_obstructed? method for horizontal and vertical with testing after cleaning up markings

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -3,68 +3,6 @@ class Piece < ApplicationRecord
   belongs_to :game , optional: true
   belongs_to :user , optional: true
 
-#   def is_occupied?(x,y)
-#     # search the pieces database and see if x, y are occupied 
-#     @piece = Piece.where(coordinate_x: x, coordinate_y: y).present?
-#   end
-
-# # Define method using x and y target coordinates to see if move is horizontal
-#   def check_horizontal(x_target, y_target)
-
-   
-#       ((coordinate_x + 1)..(x_target - 1)).each do |x|
-#         return true if is_occupied?(x, y_target)
-#       end
-#         return false    
-#   end
-
-#   # Define method using x and y target coordinates to see if move is vertical
-#   def check_vertical(x_target, y_position_change)
-#       (coordinate_y..y_position_change.abs - 1).each do |y|
-#         return true if is_occupied?(x_target, y)
-#       end
-#         return false
-#   end
-
-#   # Define method using x and y target coordinates to see if move is diaganol
-#   def check_diaganol(x_position_change, y_position_change)
-#       ((1..x_position_change)).each do |x|
-#         ((1..y_position_change)).each do |y|
-#           return true if is_occupied?(x, y)
-#         end
-#           return false
-#       end
-#   end
-
-# # Define method using x and y target coordinates to see if move is outside of the defined game board
-#   def is_path_valid?(x_target, y_target)
-#     if x_target > 7 || x_target < 0 || y_target > 7 || y_target < 0
-#       return "invalid path"
-#     end
-#   end
-
-#   def is_obstructed?(x_target, y_target)
-#     x_position_change = (x_target - coordinate_x).abs
-#     y_position_change = (y_target - coordinate_y).abs
-
-#     # is the path valid
-#     if is_path_valid?(x_target, y_target)
-#     # is the path horizontal
-#     elsif x_position_change > 0 && y_position_change == 0
-#       check_horizontal(x_target, y_target)
-#     # is the path vertical
-#     elsif x_position_change == 0 && y_position_change > 0
-#       check_vertical(x_target, y_position_change)
-#     # is the path diaganol
-#     elsif x_position_change == (y_position_change)
-#       check_diaganol(x_position_change, y_position_change) 
-#     end
-#   end
-
-#-----------------------------------------
-# jesse's code
-
-
   def is_occupied?(x,y)
     # search the pieces database and see if x, y are occupied 
       @piece = Piece.where(coordinate_x: x,  coordinate_y: y).present?
--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -57,43 +57,43 @@ RSpec.describe Piece, type: :model do
   end
 end
 
-#   describe "move_to!(new_x, new_y)" do
-#     it "should give a runtime error can't capture your own piece" do
-#       located_piece = FactoryBot.create(:piece,
-#                         coordinate_x: 3,
-#                         coordinate_y: 1,
-#                         piece_color: 'black', user_id: 1)
-#       piece = FactoryBot.create(:piece,
-#                                 coordinate_x: 3,
-#                                 coordinate_y: 0,
-#                                 piece_color: 'black', user_id: 1)
-#       expect{piece.move_to!(3,1)}.to raise_error(RuntimeError)
+  describe "move_to!(new_x, new_y)" do
+    it "should give a runtime error can't capture your own piece" do
+      located_piece = FactoryBot.create(:piece,
+                        coordinate_x: 3,
+                        coordinate_y: 1,
+                        piece_color: 'black', user_id: 1)
+      piece = FactoryBot.create(:piece,
+                                coordinate_x: 3,
+                                coordinate_y: 0,
+                                piece_color: 'black', user_id: 1)
+      expect{piece.move_to!(3,1)}.to raise_error(RuntimeError)
 
-#     end
+    end
 
-#     it "should set the captured piece to true" do
-#       located_piece = FactoryBot.create(:piece,
-#                                         coordinate_x: 3,
-#                                         coordinate_y: 1,
-#                                         piece_color: 'white', user_id: 2, captured: false)
-#       piece = FactoryBot.create(:piece,
-#                                 coordinate_x: 3,
-#                                 coordinate_y: 0,
-#                                 piece_color: 'black', user_id: 1)
+    it "should set the captured piece to true" do
+      located_piece = FactoryBot.create(:piece,
+                                        coordinate_x: 3,
+                                        coordinate_y: 1,
+                                        piece_color: 'white', user_id: 2, captured: false)
+      piece = FactoryBot.create(:piece,
+                                coordinate_x: 3,
+                                coordinate_y: 0,
+                                piece_color: 'black', user_id: 1)
 
-#       expect(piece.move_to!(3,2)).to eq(located_piece.captured = true)
+      expect(piece.move_to!(3,2)).to eq(located_piece.captured = true)
 
-#     end
+    end
 
-#     it "should move and update the position" do
-#       piece = FactoryBot.create(:piece,
-#                                 coordinate_x: 3,
-#                                 coordinate_y: 0,
-#                                 piece_color: 'black', user_id: 1)
-#       piece.move_to!(6,3)
-#       expect([piece.coordinate_x,piece.coordinate_y]).to eq([6,3])
+    it "should move and update the position" do
+      piece = FactoryBot.create(:piece,
+                                coordinate_x: 3,
+                                coordinate_y: 0,
+                                piece_color: 'black', user_id: 1)
+      piece.move_to!(6,3)
+      expect([piece.coordinate_x,piece.coordinate_y]).to eq([6,3])
 
-#     end
+    end
 
-#   end
-# end
+  end
+end
