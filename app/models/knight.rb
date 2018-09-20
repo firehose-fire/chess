80eaa88c0287f252ccc441e9806c32799411f33c
JESSE DESANTO <JESSESUS13@GMAIL.COM>
2018-09-20 08:27:29 -0700

    Add Knight valid moves and rspec tests. All tests pass

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,6 +1,7 @@
 class Knight < Piece
 
   def valid_move?(to_coordinate_x, to_coordinate_y)
+      (coordinate_x - to_coordinate_x).abs == 2 && (coordinate_y - to_coordinate_y).abs == 1 || (coordinate_x - to_coordinate_x).abs == 1 && (coordinate_y - to_coordinate_y).abs == 2
 
   end
 
