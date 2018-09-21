c2030ea27c1d8a47462f2d91cd27593f715f5130
JESSE DESANTO <JESSESUS13@GMAIL.COM>
2018-09-21 09:11:12 -0700

    Fixed failing games controller test

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -10,9 +10,9 @@ RSpec.describe GamesController, type: :controller do
       current_user = FactoryBot.build(:user)
       allow(controller).to receive(:authenticate_user!).and_return(true)
       allow(controller).to receive(:current_user).and_return(current_user)
-      game = FactoryBot.build(:game, user_black_id: user.id)
+      game = FactoryBot.build(:game, user_black_id: user.id, id: 2)
       # patch :update, params: { user_white_id: current_user.id}
-      patch :update, params: { id: game.id }
+      patch :update, params: { id: game.id, user_white_id: current_user.id }
       game.reload
       
       expect(game.user_white_id).to eq current_user.id
