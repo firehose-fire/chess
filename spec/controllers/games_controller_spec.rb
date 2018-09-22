807773442568b60c398657c289c4f96a0baba09e
JESSE DESANTO <JESSESUS13@GMAIL.COM>
2018-09-22 10:57:36 -0700

    Fixed issue with game controller spec for failing tests. Passing all tests.

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -6,14 +6,11 @@ RSpec.describe GamesController, type: :controller do
   describe "games#update action" do
     it "should update available game user white id with current user id" do
       
-      user = FactoryBot.build(:user)
-      current_user = FactoryBot.build(:user)
+      user = FactoryBot.create(:user)
+      current_user = FactoryBot.create(:user)
       allow(controller).to receive(:authenticate_user!).and_return(true)
       allow(controller).to receive(:current_user).and_return(current_user)
-      game = FactoryBot.build(:game, user_black_id: user.id, id: 2)
-      # patch :update, params: { user_white_id: current_user.id}
-      puts "game id is #{game.id}"
-      puts "current_user id is #{current_user.id}"
+      game = FactoryBot.create(:game, user_black_id: user.id)
       patch :update, params: { id: game.id, user_white_id: current_user.id }
       game.reload
       
