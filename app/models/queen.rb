e2c9e35bbbe35d9606b216c93c5119d6ae5ddc54
vlastik <vlastikczech1@gmail.com>
2018-09-22 13:15:30 -0700

    adjusted the validation for queen valid_move?

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -3,7 +3,8 @@ class Queen < Piece
   def valid_move?(x_target, y_target)
 
     if is_diagonal_move?(x_target, y_target) || is_horizontal_move?(x_target, y_target) || is_vertical_move?(x_target, y_target)
-         return is_obstructed?(x_target, y_target)
+      value = is_obstructed?(x_target, y_target)
+      value == true ? false : true
     end
 
   end
--- spec/models/queen_spec.rb
+++ spec/models/queen_spec.rb
@@ -6,20 +6,20 @@ RSpec.describe Queen, type: :model do
   piece = FactoryBot.create(:piece, coordinate_x: 6, coordinate_y: 0)
 
   describe "valid queen move?" do
-    it "should check the horizontal move and return true" do
-      expect(queen.valid_move?(5,0)).to eq(true)
+    it "should check the horizontal move and return false" do
+      expect(queen.valid_move?(5,0)).to eq(false)
     end
 
     it "should check the vertical move and return true" do
-      expect(queen.valid_move?(3,3)).to eq(true)
+      expect(queen.valid_move?(3,3)).to eq(false)
     end
 
     it "should check the diagonal move and return true" do
-      expect(queen.valid_move?(4,1)).to eq(true)
+      expect(queen.valid_move?(4,1)).to eq(false)
     end
 
-    it "should return true if there's an obstruction between the move" do
-      expect(queen.valid_move?(7,0)).to eq(true)
+    it "should return false if there's an obstruction between the move" do
+      expect(queen.valid_move?(7,0)).to eq(false)
     end
   end
 end
