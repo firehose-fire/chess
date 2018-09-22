fd3e4771995ec2aadb905948b72db4ca31b58000
vlastik <vlastikczech1@gmail.com>
2018-09-22 12:25:54 -0700

    Added valid move validation

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -1,2 +1,6 @@
 class Queen < Piece
+
+  def valid_move?(x_target, y_target)
+    is_obstructed?(x_target, y_target) ? true : false
+  end
 end
--- spec/models/queen_spec.rb
+++ spec/models/queen_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Queen, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  queen = FactoryBot.create(:queen, coordinate_x: 3, coordinate_y: 0)
+  piece = FactoryBot.create(:piece, coordinate_x: 6, coordinate_y: 0)
+
+  describe "valid queen move?" do
+    it "should check the horizontal move and return true" do
+      expect(queen.valid_move?(5,0)).to eq(true)
+    end
+
+    it "should check the vertical move and return true" do
+      expect(queen.valid_move?(3,3)).to eq(true)
+    end
+
+    it "should check the diagonal move and return true" do
+      expect(queen.valid_move?(4,1)).to eq(true)
+    end
+
+    it "should return true if there's an obstruction between the move" do
+      expect(queen.valid_move?(7,0)).to eq(true)
+    end
+  end
 end
