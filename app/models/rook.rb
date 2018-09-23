0b3a07483475ce9e26b8ff81d7099d38aac91fec
JESSE DESANTO <JESSESUS13@GMAIL.COM>
2018-09-23 09:07:55 -0700

    refactor rook valid move method. Tests pass

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,17 +1,14 @@
 class Rook < Piece
 
   def valid_move?(to_coordinate_x, to_coordinate_y)
-      #check vertical move
-          if to_coordinate_y > 0 && (coordinate_x - to_coordinate_x).abs == 0 
-        true
-      #check horizontal move
-      elsif to_coordinate_x < 8 && (coordinate_y - to_coordinate_y).abs == 0 
-        true
+      is_vertical_move? = to_coordinate_y > 0 && (coordinate_x - to_coordinate_x).abs == 0
+      is_horizontal_move? = to_coordinate_x > 0 && (coordinate_y - to_coordinate_y).abs == 0 
+  
+      if is_vertical_move? || is_horizontal_move?
+          is_obstructed?(to_coordinate_x, to_coordinate_y) == true ? false : true          
       else
-        false
+        false         
       end
-
-      
 
   end
 
--- spec/models/rook_spec.rb
+++ spec/models/rook_spec.rb
@@ -5,13 +5,13 @@ RSpec.describe Rook, type: :model do
   before(:all) do 
     game = FactoryBot.build(:game)
     user = FactoryBot.build(:user)
-    @rook = FactoryBot.build(:rook, coordinate_x: 0, coordinate_y: 3, game_id: game.id, user_id: user.id)      
+    @rook = FactoryBot.build(:rook, coordinate_x: 1, coordinate_y: 3, game_id: game.id, user_id: user.id)      
   end
 
   describe "rook valid_move?" do
     
     it "should return true if the rook piece move is valid (vertical)" do
-      expect(@rook.valid_move?(0,7)).to eq(true)
+      expect(@rook.valid_move?(1,5)).to eq(true)
     end
 
     it "should return true if the rook piece move is valid (horizontal)" do
@@ -19,12 +19,15 @@ RSpec.describe Rook, type: :model do
     end
 
     
-    # it "should return false if the rook piece move is not valid " do
-    #   expect(@rook.valid_move?(1,1)).to eq(false)
-    #   expect(@rook.valid_move?(3,3)).to eq(false)
-    # end
+    it "should return false if the rook piece move is not valid " do
+      expect(@rook.valid_move?(0,4)).to eq(false)
+      expect(@rook.valid_move?(2,2)).to eq(false)
+    end
 
-   
+    it "should return false if move is valid vertical but obstructed" do
+      obstructing_piece = FactoryBot.build(:piece, coordinate_x: 2, coordinate_y: 5)
+      expect(@rook.valid_move?(2,7)).to eq(false)
+    end
 
     
 
