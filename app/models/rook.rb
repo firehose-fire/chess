16485f27fb3539665f2ab891a5607a85545a99d8
JESSE DESANTO <JESSESUS13@GMAIL.COM>
2018-09-23 12:05:07 -0700

    Add bishop valid move method

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,12 +1,2 @@
 class Rook < Piece
-
-  def valid_move?(to_coordinate_x, to_coordinate_y)
-    if is_vertical_move?(to_coordinate_x, to_coordinate_y) || is_horizontal_move?(to_coordinate_x, to_coordinate_y)
-        is_obstructed?(to_coordinate_x, to_coordinate_y) == true ? false : true          
-    else
-      false         
-    end
-
-  end
-
 end
