233c193e5b716dcde3af76d163bde23441f34c1f
Jesse DeSanto <J.T.DeSanto@gmail.com>
2018-09-24 10:00:22 -0700

    Valid move rook (#52)
    * Define valid_move? for all pieces
    
    * Add Rook valid move method and test specs
    
    * Add Knight valid moves and rspec tests. All tests pass
    
    * Add knight valid moves and test suite. Passes all tests
    
    * Fixed failing games controller test
    
    * Fixed failing games controller test
    
    * Another try fixing failing games controller test. Changed some factory settings. Passing on local machine.
    
    * Trying fixing bug in factory.rb
    
    * Fixed issue with game controller spec for failing tests. Passing all tests.
    
    * Add rook tests
    
    * Add rook valid moves
    
    * refactor rook valid move method. Tests pass
    
    * Refactor rook valid move method. Passes tests. Fix error from factories.rb game factory

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,2 +1,8 @@
 class Knight < Piece
+
+  def valid_move?(to_coordinate_x, to_coordinate_y)
+      (coordinate_x - to_coordinate_x).abs == 2 && (coordinate_y - to_coordinate_y).abs == 1 || (coordinate_x - to_coordinate_x).abs == 1 && (coordinate_y - to_coordinate_y).abs == 2
+
+  end
+
 end
--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -77,9 +77,9 @@ RSpec.describe Piece, type: :model do
     end
 
      it "should return false if there is NOT piece obstructing the horizontal left to right" do
-      piece = FactoryBot.create(:piece, coordinate_x: 3, coordinate_y: 0 )
-      piece_obstruction = FactoryBot.create(:piece, coordinate_x: 0, coordinate_y: 3)
-      expect(piece.is_obstructed?(5,0)).to eq(false)
+      piece = FactoryBot.create(:piece, coordinate_x: 3, coordinate_y: 3 )
+      piece_obstruction = FactoryBot.create(:piece, coordinate_x: 0, coordinate_y: 2)
+      expect(piece.is_obstructed?(5,3)).to eq(false)
     end
 
     #DIAGANOL
