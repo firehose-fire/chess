a06de670280ce3715e681618d91192311419ebf3
Mark Cates <cates_mark@hotmail.com>
2018-09-24 12:43:56 -0500

    completed diagonal and added direction for both color pieces

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -2,6 +2,14 @@ class Pawn < Piece
 
   attr_accessor :has_moved
 
+  def black_piece_move?(x, y)
+    (y - coordinate_y) > 1
+  end
+
+  def white_piece_move?( x, y)
+    (y - coordinate_y) < 6
+  end
+
   def move_one_space?(x, y)
     (x - coordinate_x).abs < 1 && (y - coordinate_y).abs == 1
   end
@@ -10,15 +18,17 @@ class Pawn < Piece
     (x - coordinate_x).abs < 1 && (y - coordinate_y).abs == 2 unless has_moved == true
   end
 
-  def capture_diagonal?(x, y)
-    if check_diaganol(x, y) && single_diagonal_move?(x, y)
-      move_to?(x, y)
-    end
-  end
-
   def single_diagonal_move?(x, y)
     (x - coordinate_x).abs == 1 && (y - coordinate_y).abs == 1
   end
+
+  def capture_diagonal?(x, y)
+    if check_diaganol(x, y) && single_diagonal_move?(x, y)
+      move_to!(x, y)
+    end
+  end
+
+ 
 
   def move_to!(new_x, new_y)
     # how super works: go to the Piece class - execute the move_to! method and when complete
--- spec/models/pawn_spec.rb
+++ spec/models/pawn_spec.rb
@@ -1,55 +1,69 @@
 require 'rails_helper'
 
 RSpec.describe Pawn, type: :model do
-  
-    
-    
-    @pawn3 = FactoryBot.build(:pawn, piece_color: 'white', coordinate_x: 6, coordinate_y: 1)
-    @pawn4 = FactoryBot.build(:pawn, piece_color: 'black', coordinate_x: 7, coordinate_y: 6)
-    @pawn4.update_attributes(coordinate_y: 2)
-  
-
   def build_game_and_user
     game = FactoryBot.build(:game, id: 4)
-    user = FactoryBot.build(:user, id: 4)
+    user_white = FactoryBot.build(:user, id: 4)
+    user_black = FactoryBot.build(:user, id: 5)
   end
 
   describe 'move_one_space?' do
     it 'return true if the pawn can move one space' do
-      @pawn = FactoryBot.build(:pawn, coordinate_x: 0, coordinate_y: 6)
-      expect(@pawn.move_one_space?(0, 5)).to eq(true)
+      pawn = FactoryBot.build(:pawn, coordinate_x: 0, coordinate_y: 6)
+      expect(pawn.move_one_space?(0, 5)).to eq(true)
     end
 
     it 'return false if the pawn moves more than one space' do
-      @pawn = FactoryBot.build(:pawn, coordinate_x: 0, coordinate_y: 6)
-      expect(@pawn.move_one_space?(0, 4)).to eq(false)
+      pawn = FactoryBot.build(:pawn, coordinate_x: 0, coordinate_y: 6)
+      expect(pawn.move_one_space?(0, 4)).to eq(false)
     end
   end
 
   describe 'move_two_spaces?' do
     it 'return true if the pawn moves 2 spaces' do
-      @pawn = FactoryBot.build(:pawn, coordinate_x: 0, coordinate_y: 1)
-      expect(@pawn.move_two_spaces?(0, 3)).to eq(true)
+      pawn = FactoryBot.build(:pawn, coordinate_x: 0, coordinate_y: 1)
+      expect(pawn.move_two_spaces?(0, 3)).to eq(true)
     end
 
     it 'return false if the pawn moves 3 spaces' do
-      @pawn = FactoryBot.build(:pawn, coordinate_x: 0, coordinate_y: 1)
-      expect(@pawn.move_two_spaces?(0, 4)).to eq(false)
+      pawn = FactoryBot.build(:pawn, coordinate_x: 0, coordinate_y: 1)
+      expect(pawn.move_two_spaces?(0, 4)).to eq(false)
     end
 
     it 'return false if the pawn moves 2 spaces after a first move' do
-      @pawn = FactoryBot.create(:pawn, coordinate_x: 5, coordinate_y: 1)
-      @pawn.update_attributes(coordinate_y: 2)
-      expect(@pawn.move_two_spaces?(5, 3)).to eq(false)
+      pawn = FactoryBot.create(:pawn, coordinate_x: 5, coordinate_y: 1)
+      pawn.update_attributes(coordinate_y: 2)
+      expect(pawn.move_two_spaces?(5, 3)).to eq(false)
     end
   end
 
   describe 'capture_diagonal?' do
     it 'return true if can capture a diagonal' do
-      pawn_white = FactoryBot.build(:pawn, piece_color: 'white', coordinate_x: 6, coordinate_y: 1)
-      pawn_black = @pawn4 = FactoryBot.build(:pawn, piece_color: 'black', coordinate_x: 7, coordinate_y: 6)
+      pawn_white = FactoryBot.build(:pawn, piece_color: 'white', coordinate_x: 6, coordinate_y: 1, user_id: 4)
+      pawn_black = FactoryBot.build(:pawn, piece_color: 'black', coordinate_x: 7, coordinate_y: 6, user_id: 5)
       pawn_black.update_attributes(coordinate_y: 2)
       expect(pawn_white.capture_diagonal?(7, 2)).to eq(true)
+    end
+
+    it 'return false if can capture a diagonal' do
+      pawn_white = FactoryBot.build(:pawn, piece_color: 'white', coordinate_x: 5, coordinate_y: 1, user_id: 4)
+      pawn_black = FactoryBot.build(:pawn, piece_color: 'black', coordinate_x: 4, coordinate_y: 6, user_id: 5)
+      pawn_black.update_attributes(coordinate_y: 2)
+      expect(pawn_white.capture_diagonal?(4, 2)).to eq(true)
+    end
+
+    it 'return false if can capture a diagonal' do
+      pawn_white = FactoryBot.build(:pawn, piece_color: 'white', coordinate_x: 4, coordinate_y: 6, user_id: 4)
+      pawn_black = FactoryBot.build(:pawn, piece_color: 'black', coordinate_x: 3, coordinate_y: 1, user_id: 5)
+      pawn_black.update_attributes(coordinate_y: 5)
+      expect(pawn_white.capture_diagonal?(3, 5)).to eq(true)
+    end
+
+    it 'return false if can capture a diagonal' do
+      pawn_white = FactoryBot.build(:pawn, piece_color: 'white', coordinate_x: 4, coordinate_y: 6, user_id: 4)
+      pawn_black = FactoryBot.build(:pawn, piece_color: 'black', coordinate_x: 5, coordinate_y: 1, user_id: 5)
+      pawn_black.update_attributes(coordinate_y: 5)
+      expect(pawn_white.capture_diagonal?(5, 5)).to eq(true)
     end
   end
 end
