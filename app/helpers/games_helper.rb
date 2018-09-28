ba8e065a500cf63465538a2b340bbb8de519f954
Eric Knight <35003931+HortusHonu@users.noreply.github.com>
2018-09-28 19:42:12 -0400

    Wip mover (#63)
    * master after db:drop, create and migrate
    
    * support moving pieces so far
    
    * add helpers and links to views - fix controllers to show pieces and highlighted square
    
    * try to get piece to move
    
    * Test
    
    * need pieces update to work
    
    * before push for mark
    
    * swap row and column
    
    * fix css conflict
    
    * uncomment @selected_piece=nil
    
    * remove classes from table in css and swap column & row in td data
    
    * swap column & row in td data in pieces view
    
    * reapply highlighted piece and align piece to bottom and top of board
    
    * switch from form to helpers
    
    * gets to pieces controller update then errors
    
    * get piece id to pieces controller, need x & y to update
    
    * gets to pieces update - need to pass the x and y coords in
    
    * move piece with a form - pieces update change
    
    * add move_to! to pieces update controller

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -3,4 +3,23 @@ module GamesHelper
   def piece_image(type, color)
     image_tag "#{color}-#{type}.svg"
   end
+
+  # Find all of the squares that do not have pieces in them!
+
+  def get_piece_at_click(pieces, row, column)
+    pieces.find do |piece|
+      piece_in_row_and_column?(piece, row, column)    
+    end
+  end
+
+  # Find all of the squares that do not have pieces in them!
+
+  def piece_in_row_and_column?(piece, row, column)
+    piece && row == piece.coordinate_x && column == piece.coordinate_y      
+  end
+
+  def link_to_piece(piece)
+    link_to 'piece', piece_path(piece) if piece
+  end
+
 end
---
+++ app/helpers/pieces_helper.rb
@@ -0,0 +1,21 @@
+module PiecesHelper
+
+# need to build out helpers to move to squares
+
+  def link_to_square(piece)
+    link_to 'move', piece_path(piece), :method => :patch
+  end
+
+  def get_square_at_click(piece, row, column)
+    
+  end
+
+  # def move_to_square?(square, piece, row, column)
+  #   square && row == piece.coordinate_x && column == piece.coordinate_y      
+  # end
+
+  # def not_selected_square(get_square_at_click)
+  #   link_to 'piece', piece_path(piece) if piece                                                  
+  # end
+
+end
