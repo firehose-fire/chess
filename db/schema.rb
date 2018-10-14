6ad79335688016bbdd99d7e66fd126a9a0e2f44f
Vlastik Walker <vlastikczech1@gmail.com>
2018-10-14 16:04:33 +0200

    Added the en_passant method to the pawn, and created an boolean to keep track of the state (#73)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181001183147) do
+ActiveRecord::Schema.define(version: 20181005164101) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20181001183147) do
     t.integer  "user_id"
     t.integer  "game_id"
     t.boolean  "has_moved"
+    t.boolean  "en_passant"
   end
 
   create_table "queens", force: :cascade do |t|
