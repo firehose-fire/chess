820d505799ece3b4a0f6c71181b442aff5be6d00
Eric <james.eric.knight@gmail.com>
2018-10-16 01:52:46 +0000

    modify drag & drop and and add promotion modal

--- app/assets/javascripts/dragdrop.js
+++ app/assets/javascripts/dragdrop.js
@@ -1,33 +1,80 @@
+var pieceId;
+var pieceType;
+var column;
+var row;
 
+$(document).ready(function() {
+  $(".pieces").draggable({
+    drag: function() {
+      pieceId = $(this).data("piece_id");
+    }
+  });
 
-$( function() {
-    var $piece_id
-    $( ".pieces" ).draggable({ 
-      drag: function(){
-       $piece_id = $(this).attr('id')
+  $(".droppable").droppable({
+    drop: function(event, ui) {
+      column = $(this).data("column");
+      row = $(this).data("row");
+      pieceType = $(this).data("piece_type");
 
-     }
-    });
-    $( ".droppable" ).droppable({
-      drop: function( event, ui ) {
-        var $column = $(this).data('column');
-        var $row = $(this).data('row')
-        $.ajax({
-          url: "/pieces/" + $piece_id,
-          type: "PUT",
-          dataType: "script",
-          data: { 'piece': {'coordinate_x': $row, 'coordinate_y': $column }},  
-          success: function(){           
-            window.location.reload()
-          }
-         
-        })
+      $.ajax({
+        url: "/pieces/" + pieceId,
+        type: "PUT",
+        data: { piece: { coordinate_x: row, coordinate_y: column } },
+        success: function() {
+          debugger;
+          // This where we want to move/replace the html element
+          checkPromo();
+        },
+        error: function() {
+          debugger;
+          // This is where we want to error 400
+        }
+      });
+    }
+  });
+});
 
-      
-      }
-    });
-  } );
+function checkPromo() {
+  if (
+    (pieceType == "Pawn" && column == 0) ||
+    (pieceType == "Pawn" && column == 7)
+  ) {
+    promoModal();
+  }
+}
 
+function promoModal() {
+  $("#promotion").modal("show");
+  $("#rookButton").on("click", function() {
+    promoChoice = "Rook";
+    givePromo();
+  });
+  $("#bishopButton").on("click", function() {
+    promoChoice = "Bishop";
+    givePromo();
+  });
+  $("#knightButton").on("click", function() {
+    promoChoice = "Knight";
+    givePromo();
+  });
+  $("#queenButton").on("click", function() {
+    promoChoice = "Queen";
+    givePromo();
+  });
+}
 
-
- 
+function givePromo() {
+  $.ajax({
+    url: "/pieces/" + pieceId,
+    type: "PUT",
+    data: { piece: { type: promoChoice } },
+    success: function() {
+      debugger;
+      // This where we want to move/replace the html element
+    },
+    error: function() {
+      debugger;
+      // This is where we want to error 400
+    }
+  });
+}
